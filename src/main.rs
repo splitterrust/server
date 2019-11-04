463469fe3c10e07db355c9d709150a9b3a137b36
Maximilian Konter <biskit1943@gmx.de>
2019-11-04 21:47:45 +0100

    Trying to understand serde

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder};
 use actix_web::get;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use std::fs;
 
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+mod json;
 
-fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
-}
-
-#[get("/hello")]
+#[get("/")]
 fn index3() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    let contents = fs::read_to_string("src/spells.json")
+        .expect("Something went wrong reading the file");
+    HttpResponse::Ok().body(contents)
+}
+
+fn read_spells_from_file<P: AsRef<Path>>(path: P) -> Result<json::types::Spell, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `Spell`.
+    let spell = serde_json::from_reader(reader)?;
+
+    // Return the `Spells`.
+    Ok(spell)
+}
+
+fn read_spells_from_file2(path: &str) -> Result<Vec<json::types::Spell>, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let contents = fs::read_to_string(path)
+        .expect("Failed to read File");
+
+    // Read the JSON contents of the file as an instance of `Spell`.
+    let spells: Vec<json::types::Spell> = serde_json::from_str(&contents)?;
+
+    // Return the `Spells`.
+    Ok(spells)
 }
 
 fn main() {
+    let spells = read_spells_from_file2("test.json").unwrap();
+    println!("{:#?}", spells);
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
             .service(index3)
     })
     .bind("127.0.0.1:8088")
